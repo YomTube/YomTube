774f84d833ed1b2f88a524f6e03cb2d88c0b4467
Tokxhu <thomas.granbohm@hotmail.com>
2020-02-04 14:15:08 +0100

    Working transcoding on GPU

--- src/api/routes/videos.js
+++ src/api/routes/videos.js
@@ -1,25 +1,25 @@
-import Router from 'express';
-import { Video } from '../../models/index.js';
-import multer from 'multer';
-import auth from '../middleware/auth';
-import ffmpeg from 'fluent-ffmpeg'
-import { promises as fs } from 'fs';
+import Router from "express";
+import { Video } from "../../models/index.js";
+import multer from "multer";
+import auth from "../middleware/auth";
+import ffmpeg from "fluent-ffmpeg";
+import { promises as fs } from "fs";
 
 const router = Router();
 
 const GET_PATH = "/tmp";
 const SAVE_PATH = process.env.ROOT_DIR + "videos";
 
-const upload = multer({ dest: GET_PATH })
+const upload = multer({ dest: GET_PATH });
 // Get all videos
 router.get("/", async (req, res) => {
 	try {
 		const videos = await Video.find({});
 		res.send({ videos });
 	} catch (err) {
-		res.status(400).send({ error: err.message })
+		res.status(400).send({ error: err.message });
 	}
-})
+});
 
 // Get specific video
 router.get("/:id", async (req, res) => {
@@ -32,22 +32,22 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
-router.get('/:id/video', async (req, res) => {
+router.get("/:id/video", async (req, res) => {
 	let id = req.params.id;
 	try {
-		const video = await Video.findOne({ _id: id }, 'filePath');
-		res.sendFile(video.filePath, { root: '/' });
+		const video = await Video.findOne({ _id: id }, "filePath");
+		res.sendFile(video.filePath, { root: "/" });
 	} catch (err) {
 		res.status(400).send({ error: err.message });
 	}
-})
+});
 
 // Upload video
-router.post("/", auth, upload.single('video'), async (req, res) => {
+router.post("/", auth, upload.single("video"), async (req, res) => {
 	try {
-		if (!(req.file)) throw new Error('No file was provided');
+		if (!req.file) throw new Error("No file was provided");
 		const { name, desc } = req.body;
-		const { filename, path } = req.file
+		const { filename, path } = req.file;
 		const video = new Video({
 			title: name,
 			description: desc,
@@ -57,26 +57,26 @@ router.post("/", auth, upload.single('video'), async (req, res) => {
 		await video.save();
 
 		let outputPath = `${SAVE_PATH}/${video._id}`;
-		console.log(outputPath)
+		console.log(outputPath);
 		await fs.mkdir(outputPath);
 		video.filePath = outputPath;
 
 		processVideo(`${GET_PATH}/${filename}`, video);
-		res.status(201).send({ video })
+		res.status(201).send({ video });
 	} catch (err) {
-		console.error(err)
+		console.error(err);
 		res.status(400).send({ error: err.message });
 	}
-})
+});
 export default router;
 
-
-let getRes = (path) => {
+function getRes(path) {
 	return new Promise((res, rej) => {
-		ffmpeg.ffprobe(path, function (err, metadata) {
+		ffmpeg.ffprobe(path, function(err, metadata) {
 			if (err) {
 				rej(err);
 			} else {
+				console.log(metadata.streams[0]);
 				let obj = {
 					width: metadata.streams[0].width,
 					height: metadata.streams[0].height
@@ -84,38 +84,65 @@ let getRes = (path) => {
 				res(obj);
 			}
 		});
-	})
+	});
 }
 
-
-let transcodeToRes = (path, shortSide, bitrate, videoID, portrait) => {
+function transcodeToRes(path, shortSide, bitrate, videoID, portrait) {
 	return new Promise((res, rej) => {
-		let resolution = portrait ? "?x" + shortSide : shortSide + "x?";
-		let localSavePath = SAVE_PATH + "/" + videoID + "/" + shortSide + ".mp4";
+		let localSavePath = savePath + videoID + "/" + shortSide + ".mp4";
+		let resolution = portrait ? "-1:" + shortSide : shortSide + ":-1";
+		let scaleFilter = "scale_npp=" + resolution;
 		ffmpeg()
 			.input(path)
+			.inputOption([
+				"-vsync 0",
+				"-hwaccel cuvid",
+				"-hwaccel_device 0",
+				"-c:v h264_cuvid"
+			])
+			.videoCodec("h264_nvenc")
+			.videoFilter(scaleFilter)
 			.native()
-			.audioCodec('aac')
+			.audioCodec("aac")
 			.audioBitrate(128)
 			.audioChannels(2)
-			.videoCodec('libx264')
 			.videoBitrate(bitrate)
-			.size(resolution)
 			.save(localSavePath)
-			.on('error', (err) => {
-				rej(err);
+			.on("error", err => {
+				ffmpeg()
+					.input(path)
+					.videoCodec("h264_nvenc")
+					.videoFilter(["hwupload_cuda", scaleFilter])
+					.native()
+					.audioCodec("aac")
+					.audioBitrate(128)
+					.audioChannels(2)
+					// .videoCodec('libx264')
+					.videoBitrate(bitrate)
+					// .size(resolution)
+					// .keepDAR()
+					.save(localSavePath)
+					.on("error", err => {
+						console.log(
+							"failed software transocde" + resolution
+						);
+						// console.log(err);
+						rej(err);
+					})
+					.on("end", () => {
+						res();
+					});
 			})
-			.on('end', () => {
+			.on("end", () => {
 				res();
 			});
-	})
+	});
 }
 
 let processVideo = async (path, video) => {
-	let videoID = video._id
+	let videoID = video._id;
 	return new Promise(async (res, rej) => {
-		let obj = await getRes(path)
-			.catch(error => console.error(error));
+		let obj = await getRes(path).catch(error => console.error(error));
 
 		let qualities = [
 			{
@@ -140,27 +167,39 @@ let processVideo = async (path, video) => {
 			},
 			{
 				res: 360,
-				bitrate: 1000,
+				bitrate: 1000
 			},
 			{
 				res: 240,
-				bitrate: 500,
+				bitrate: 500
 			}
-		]
+		];
 
 		try {
 			if (obj) {
 				let portrait = obj.width <= obj.height;
 				let maxest = Math.max(obj.width, obj.height);
-				let available_qualities = qualities.filter(quality => maxest >= quality.res);
-				await Promise.all(available_qualities.map(quality => transcodeToRes(path, quality.res, quality.bitrate, videoID, portrait)))
+				let available_qualities = qualities.filter(
+					quality => maxest >= quality.res
+				);
+				await Promise.all(
+					available_qualities.map(quality =>
+						transcodeToRes(
+							path,
+							quality.res,
+							quality.bitrate,
+							videoID,
+							portrait
+						)
+					)
+				);
 				await fs.unlink(path);
-				console.log("Finished transcoding for %s", videoID)
+				console.log("Finished transcoding for %s", videoID);
 				await video.save();
 				return res();
 			}
 		} catch (err) {
 			rej(err);
 		}
-	})
-}
+	});
+};
